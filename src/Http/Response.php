bf47a8629e51d430cd873985e449359b05dcc257
Fede Isas <federicoisas@gmail.com>
2017-06-22 20:39:38 -0300

    Array casting of json_decode

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -23,7 +23,7 @@ class Response
      */
     public function __construct($statusCode, $json = '{}')
     {
-        $this->data = json_decode($json, true);
+        $this->data = (array) gijson_decode($json, true);
         $this->statusCode = (int) $statusCode;
     }
 
